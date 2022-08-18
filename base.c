394e89cf642e28061b9c7f5046744eb68cbfa6ce
MatheusZCAR <107090798+MatheusZCAR@users.noreply.github.com>
2022-08-18 16:00:58 -0300

    atualizado
    propaganda necessária para não afetar o Grêmio

--- base.c
+++ base.c
@@ -9,5 +9,6 @@ int main()
 printf("com vc esta");
 printf("ola");
 printf("Hello World\n");
+printf("like e favorito, meta de 10 mil likes inscreva-se agora mesmo (caso contrário, vc nn é legal)");
 return 0;
 }
